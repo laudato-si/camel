de61bde4829d10c60327db01e9e067463d246563
laudato-si <intelgdev@outlook.com>
2023-09-02 12:06:18 +0200

    change baseurl to dataplicity

--- src/app/api/url.js
+++ src/app/api/url.js
@@ -1,4 +1,5 @@
 export default {
     // baseUrl: "http://127.0.0.1:8000/",
-    baseUrl: "http://192.168.1.154/women/",
+    // baseUrl: "http://192.168.1.154/women/",
+    baseUrl: "https://unmated-ox-1276.dataplicity.io/women/",
   };
